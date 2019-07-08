c775935bdb624696e7ea946f4d871ac069ecbe0a
Elaguy <aschultz086@gmail.com>
2019-07-08 18:33:10 -0400

    Current Debt Prototype

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Allows YAL activists to easily see the US National Debt for Visualize the Debt e
 View the current US National Debt at a particular moment, and update it upon request. This makes it easier for YAL activists to show Visualize the Debt participants how fast the US National Debt is increasing.
 
 Contributions from other YAL members with programming skills are welcome!
+
+Uses TreasuryDirect Web API (https://www.treasurydirect.gov/webapis/webapisindex.htm)
--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 function update() {
-	document.getElementById("diffValue").innerHTML = "Difference from previous value = $34000";
+	var request = new XMLHttpRequest();
+	var proxyURL = "https://cors-anywhere.herokuapp.com/";
+	var requestURL = "https://www.treasurydirect.gov/NP_WS/debt/current?format=json";
+	
+	request.open("GET", proxyURL + requestURL);
+	request.responseType = "json";
+	request.send();
+	
+	request.onload = function() {
+		var data = request.response;
+		var debtValue = data["totalDebt"].toString();
+		
+		var formattedDebtValue = debtValue.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+		
+		var debtValueHTML = document.getElementById("debtValue");
+		
+		debtValueHTML.innerHTML = "$" + formattedDebtValue;
+		
+		console.log("effectiveDate: " + data["effectiveDate"]);
+		console.log("totalDebt: " + data["totalDebt"]);
+	}
 }
