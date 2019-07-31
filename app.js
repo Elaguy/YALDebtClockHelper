8dfefcc40b0fcdc3f06c0f7a4305fcd11d4fde25
Elaguy <aschultz086@gmail.com>
2019-07-31 18:22:03 -0400

    Calculates average per second rate

--- app.js
+++ app.js
@@ -1,23 +1,87 @@
-function update() {
-	var request = new XMLHttpRequest();
-	var proxyURL = "https://cors-anywhere.herokuapp.com/";
-	var requestURL = "https://www.treasurydirect.gov/NP_WS/debt/current?format=json";
+var App = function() {
+	var app = this;
+
+	app.run = function() {
+		app.getRate();
+	};
+
+	// returns per second average rate
+	app.getRate = function() {
+		var request = new XMLHttpRequest();
+		var proxyURL = "https://cors-anywhere.herokuapp.com/";
+		
+		var currentDate = new Date();
+		
+		/*
+			Variables are "returned" from the method like this
+			because the onload function down below causes problems
+			when trying to actually return things in this function.
+			
+			I blame JavaScript.
+		*/
+		
+		var startDate = app.getStartDate(currentDate);
+		var endDate = app.getCurrentDate(currentDate);
+		
+		var requestURL = "https://www.treasurydirect.gov/NP_WS/debt/search?startdate=" 
+			+ startDate + "&enddate=" + endDate + "&format=json";
 	
-	request.open("GET", proxyURL + requestURL);
-	request.responseType = "json";
-	request.send();
+		request.open("GET", proxyURL + requestURL);
+		request.responseType = "json";
+		request.send();
+		
+		/*
+			Is the only way to make sure the program is waiting 
+			for the request to finish) is having the rest of the 
+			functions be executed in this function?
+			
+			Using this.request did not work, kept getting
+			"this.request is undefined" errors.
+		*/
+		request.onload = function() {
+			var data = request.response;
+			
+			var entriesLength = data["entries"].length;
+			var latestDebt = data["entries"][0]["totalDebt"];
+			var latestDebtDate = ["entries"][0]["effectiveDate"];
+			
+			var initialDebt = data["entries"][entriesLength-1]["totalDebt"];
+			
+			var secRate = (latestDebt - initialDebt)/(entriesLength * 86400);
+			
+			console.log("requestURL: " + requestURL);
+			console.log("latestDebt: " + latestDebt);
+			console.log("latestDebtDate: " + latestDebtDate);
+			console.log("initialDebt: " + initialDebt);
+			console.log("entriesLength: " + entriesLength);
+			console.log("secRate: " + secRate);
+			console.log("requestStatus: " + request.status);
+			
+			app.showDebt(latestDebt, latestDebtDate, secRate);
+		}
+	};
 	
-	request.onload = function() {
-		var data = request.response;
-		var debtValue = data["totalDebt"].toString();
+	app.showDebt = function(latestDebt, latestDebtDate, secRate) {
 		
-		var formattedDebtValue = debtValue.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+	};
+	
+	app.getStartDate = function(currentDate) {
+		if((currentDate.getMonth()+1) <= 9 && currentDate.getDate() <= 31) {
+			return (currentDate.getFullYear()-1) + "-" + "10" + "-" + "1";
+		}
 		
-		var debtValueHTML = document.getElementById("debtValue");
+		else {
+			return currentDate.getFullYear() + "-" + "10" + "-" + "1";
+		}
 		
-		debtValueHTML.innerHTML = "$" + formattedDebtValue;
-		
-		console.log("effectiveDate: " + data["effectiveDate"]);
-		console.log("totalDebt: " + data["totalDebt"]);
-	}
-}
+		return "";
+	};
+	
+	app.getCurrentDate = function(currentDate) {
+		return currentDate.getFullYear() + "-" + (currentDate.getMonth()+1)
+			+ "-" + currentDate.getDate();
+	};
+};
+
+var app = new App();
+window.onload = app.run();
